2c178ecfc3253560549f378e75eb035d682ff6c4
paneee <dudczyk@o2.pl>
2021-07-01 18:26:25 +0100

    second

--- app.py
+++ app.py
@@ -1,23 +1,38 @@
 from flask import Flask, redirect, url_for, render_template
 from flask_bootstrap import Bootstrap
 import os
-# from model import WebRadios
 
 app = Flask(__name__) 
 app.secret_key = os.urandom(12)
 
-
+# WebRadios={"ChiliZet":"https://ch.cdn.eurozet.pl/chi-net.mp3",
+# "357":"https://n06a-eu.rcs.revma.com/ye5kghkgcm0uv?rj-ttl=5&rj-tok=AAABduJfGVcAbh2i1fQT0iMZcA",
+# "RNS":"https://stream.nowyswiat.online/aac",
+# "Record Chillout":"http://air2.radiorecord.ru:805/chil_aac_64",
+# "Radio Kampus":"http://193.0.98.66:8005/",
+# "Weszlo":"http://radio.weszlo.fm/s7d70a7895/listen",
+# "PR3 Trojka":"mms://stream.polskieradio.pl/program3"}
 
 Bootstrap(app)
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 
-# print (WebRadios.items)
 
 @app.route('/', methods=['GET','POST'])
 def hello():
-    #return "Hello World"
+    # print(WebRadios)
     return render_template('index.html')
 
-app.run(debug=True, port=5010, host='192.168.1.3')  
+app.run(debug=True, port=5000, host='192.168.1.3')  
+
+
+
+
+
+
+
+
+
+
+
 
 
