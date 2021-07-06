a6de8732a205ae758fe19a6fe749ad97f72c587e
paneee <dudczyk@o2.pl>
2021-07-06 09:09:37 +0100

    Refactoring API

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, redirect, url_for, render_template, request
+from typing import Any
+from flask import Flask, json, redirect, url_for, render_template, request
+import flask
 from flask_bootstrap import Bootstrap
-from model import WebRadios
+from model import WebRadioEncoder, WebRadios
+from model import WebRadiosList
 from mpc import Mpc
 import os
 from flask_restful import Resource, Api
-from flask import jsonify
+from flask import jsonify 
 
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
@@ -16,31 +19,65 @@ app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 
 api = Api(app)
 
+class GetAllStation2(Resource):
+    def get(self):
+        return WebRadioEncoder().encode(WebRadiosList)
+
 class GetAllStation(Resource):
     def get(self):
-        return WebRadios
+        resp = flask.Response(WebRadioEncoder().encode(WebRadiosList))
+        resp.headers['Content-Type'] = 'application/json; charset=utf-8'
+        resp.headers['Access-Control-Allow-Origin'] = '	*'
+        resp.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS'
+        resp.headers['Access-Control-Allow-Headers'] = 'Content-Type, Origin, Accept, Authorization, Content-Length, X-Requested-With'
+        return resp
 
 class GetVolume(Resource):
     def get(self):
-        return mpc.getVolume()
+        resp = flask.Response(mpc.getVolume())
+        resp.headers['Content-Type'] = 'application/json; charset=utf-8'
+        resp.headers['Access-Control-Allow-Origin'] = '	*'
+        resp.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS'
+        resp.headers['Access-Control-Allow-Headers'] = 'Content-Type, Origin, Accept, Authorization, Content-Length, X-Requested-With'
+        return resp
+
 
 class SetVolume(Resource):
     def post(self, value):
         mpc.volumeChange(value)
-        return mpc.getVolume(), 201
+        resp = flask.Response(mpc.getVolume())
+        resp.headers['Content-Type'] = 'application/json; charset=utf-8'
+        resp.headers['Access-Control-Allow-Origin'] = '	*'
+        resp.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS'
+        resp.headers['Access-Control-Allow-Headers'] = 'Content-Type, Origin, Accept, Authorization, Content-Length, X-Requested-With'
+        return resp, 201
+
 
 class Play(Resource):
     def post(self, station):
         mpc.play(WebRadios[station])
-        return {station,WebRadios[station]}, 201
+        resp = flask.Response("OK")
+        resp.headers['Content-Type'] = 'application/json; charset=utf-8'
+        resp.headers['Access-Control-Allow-Origin'] = '	*'
+        resp.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS'
+        resp.headers['Access-Control-Allow-Headers'] = 'Content-Type, Origin, Accept, Authorization, Content-Length, X-Requested-With'
+        return resp, 201
+        #return {station,WebRadios[station]}, 201
+
 
 class Stop(Resource):
     def post(self):
         mpc.stop()
-        return "OK", 201
+        resp = flask.Response("OK")
+        resp.headers['Content-Type'] = 'application/json; charset=utf-8'
+        resp.headers['Access-Control-Allow-Origin'] = '	*'
+        resp.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS'
+        resp.headers['Access-Control-Allow-Headers'] = 'Content-Type, Origin, Accept, Authorization, Content-Length, X-Requested-With'
+        return resp, 201
 
         
 api.add_resource(GetAllStation, '/api/getAllStation')
+api.add_resource(GetAllStation2, '/api/getAllStation2')
 api.add_resource(GetVolume, '/api/getVolume')
 api.add_resource(SetVolume,'/api/setVolume/<value>')
 api.add_resource(Play,'/api/Play/<station>')
@@ -73,7 +110,7 @@ def hello():
 
 
 
-app.run(debug=True,port=5000, host='0.0.0.0')
+app.run(debug=True,port=8080, host='0.0.0.0')
 
 
 
